53f67153140c692afed0ff58f28b41e51552674d
lemev <lemev@LAPTOP-33SNF0G7>
2023-11-06 14:00:40 +0100

    TEST FINAL

---
+++ testgit/src/testgit/Color.java
@@ -0,0 +1,12 @@
+package testgit;
+
+public class Color {
+
+	public static final Color RED = null;
+
+	public char[] getRed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ testgit/src/testgit/Testfinal.java
@@ -0,0 +1,9 @@
+package testgit;
+
+public class Testfinal {
+	  public static void main(String[] args) {
+	    Color c = Color.RED;
+	    System.out.println(c.getRed());
+	  }
+	}
+
